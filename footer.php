7f76995425b96b6f7f89ebf0d2d3697d2808021a
Sandeep Yadav <63005190+sandeepyadav12@users.noreply.github.com>
2022-05-11 21:55:35 +0545

    footer.php and register.php added

---
+++ footer.php
@@ -0,0 +1,77 @@
+    <footer style="margin-bottom:0px;">
+        <div class="container-fluid text-center foot" id="foot">
+            <div class=" row">
+                <div class="footcon col-md-4">
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Contact us</h5>
+                    <br>
+                        
+
+                    <ul class="list-unstyled">
+                        <li>
+                            Location: Cleckhudderfax,United Kingdom
+
+                        </li>
+                        <br>
+                        <li>
+                            Phone: +0768942122
+                        </li>
+                        <br>
+                            
+                        <li>
+                            Email: Cleckdiced2021@gmail.com
+                        </li>
+                        <br>
+                    </ul>
+
+                </div>
+                <div class=" footcon col-md-4">
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">About us</h5>
+                    <br>
+                    <ul class="list-unstyled">
+                        <li>
+                         Cleckdiced is the online platform for all traders and customer around cleckhudderfax area helping to selling and buying goods.                        </li>
+                    </ul>
+
+                </div>
+                <div class="footcon col-md-4">
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Menu</h5>
+                    <br>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="index.php" style="color:white;">Home</a>
+                        </li>
+                        <br>
+                        <li>
+                            <a href="shop.php"  style="color:white;">MyProducts</a>
+                        </li>
+                        <li>
+                        <br>
+                            <a href="traderreg.php"  style="color:white;">Register as Trader</a>
+                        </li>
+                        <li>
+                        <br>
+                            <a href="register.php"  style="color:white;">Register as Customer</a>
+                        </li>
+                    </ul>
+                </div>
+    
+            </div>
+
+            <div class="social">
+                    <h5 style="color:white;" class="font-weight-bold  mt-3 mb-4">Socials</h5>
+
+                <div class="justify-content-center">
+                    <a href="https://instagram.com"><i class="fab fa-instagram "></i></a>
+                    <a href="https://facebook.com"><i class="fab fa-facebook"></i></a>
+                    <a href="https://plus.google.com"><i class="fab fa-google-plus"></i></a>
+                </div>
+            </div>
+            <hr>
+            <h5 style="color:white;">© 2020 Copyrignt: Cleckdiced2021.com, All Rights Reserved.</h5>
+        </div>
+    </footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
