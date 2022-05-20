c1e40f5b3b728cb4677cc9bd79061557c7315afa
Sandeep Yadav <63005190+sandeepyadav12@users.noreply.github.com>
2022-05-20 13:28:04 +0545

    changesssssss

--- index.php
+++ index.php
@@ -45,6 +45,9 @@
                         <div class="carousel-item">
                             <img class="d-block w-100" src="./assets/img/Banner Swiper/vegetables.png" alt="Fourth slide">
                         </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="./assets/img/Banner Swiper/delicatessen.png" alt="Fifth slide">
+                        </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
