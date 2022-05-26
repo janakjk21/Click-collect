ea1e608990eaf598abb89543b6cce7072557f85a
Sandeep Yadav <63005190+sandeepyadav12@users.noreply.github.com>
2022-05-26 15:19:05 +0545

    invoice

--- invoice.php
+++ invoice.php
@@ -16,7 +16,7 @@ $active = 'Account';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>CleckDiced</title>
+    <title>Invoice - Click & Collect Groceries</title>
     <link rel="stylesheet" href="Styles/bootstrap-337.min.css">
     <link rel="stylesheet" href="Styles/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
